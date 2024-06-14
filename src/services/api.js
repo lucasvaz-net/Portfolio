39d0f3dfdac7058130bb17f38568cdd682e8f8ff
Lucas Vaz <110433221+lucasvaz-net@users.noreply.github.com>
2024-06-14 14:31:07 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://localhost:7056/api',
+  baseURL: 'https://portfolioapi.lucasvaz.dev.br/api',
   headers: {
     'Content-Type': 'application/json'
   }
